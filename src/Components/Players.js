7416b087967658697b61396c7dccbc7574ff5a39
Christopher Terrano <mterrano1@yahoo.com>
2022-05-02 14:20:35 -0500

    create players page

---
+++ src/Components/Players.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Players() {
+    return (
+        <div>
+            <hr/>
+            <h1>Players Page</h1>
+        </div>
+    );
+}
+
+export default Players;
