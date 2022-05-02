09625289c308f0a39df56c1a667a784f01d1e3b4
Christopher Terrano <mterrano1@yahoo.com>
2022-05-02 14:22:25 -0500

    create player page

---
+++ src/Components/Player.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Player() {
+    return (
+        <div>
+            <hr/>
+            <h1>Player Page</h1>
+        </div>
+    );
+}
+
+export default Player;
