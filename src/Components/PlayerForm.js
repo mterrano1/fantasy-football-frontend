5de96b43a89e814a2a5e56cc3a73a26288ea8ed5
Christopher Terrano <mterrano1@yahoo.com>
2022-05-02 14:28:49 -0500

    create playerform page

---
+++ src/Components/PlayerForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PlayerForm() {
+    return (
+        <div>
+            <hr/>
+            <h1>PlayerForm Page</h1>
+        </div>
+    );
+}
+
+export default PlayerForm;
