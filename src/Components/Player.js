4d401c34aec2c0548d3b78cb0c124b202e31a349
Christopher Terrano <mterrano1@yahoo.com>
2022-05-17 16:16:55 -0500

    create player display for selected player

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { MyContext } from '../Context/MyContext';
+import PlayerInfo from './PlayerInfo';
 
-function Player() {
+const Player = () => {
+    const { id } = useParams();
+    const { players } = useContext(MyContext);
+
+    //find player that matches params id
+    const findPlayer = players.filter(player => player.id == id)
+
+    const displayedPlayer = findPlayer.map(player => (
+        <PlayerInfo key={player.id} player={player} />
+    ));
+
+
     return (
-        <div>
-            <hr/>
-            <h1>Player Page</h1>
+        <div className='playerContainer'>
+            {displayedPlayer}
         </div>
     );
 }
